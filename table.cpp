2c98192ed7599a52dc73432ee4b5ff9893f9682b
Marco Brizzio <mbrizzio@umich.edu>
2025-06-16 15:31:25 -0300

    Split table.cpp into a .cpp and .h file

--- table.cpp
+++ table.cpp
@@ -1,171 +1,160 @@
-#include <optional>
-#include <map>
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <variant>
-#include <cassert>
-#include <tuple>
+#include "datatypes.h"
+#include "table.h"
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-using Types = variant<
-  int,        //INTEGER
-  int16_t,    //SMALLINT
-  int64_t,    //BIGINT
-  float,      //FLOAT
-  string,     //TEXT
-  monostate   //NULL
->;
 
-using Row = unordered_map<string, Types>;
-using Column = vector<Types>;
+// Public
 
-monostate null = monostate{};
+Table::Table() {}
 
-class Table{
-  public:
+// Add a new column to the table
+// Ensures the datatype is supported
+void Table::insertColumn(const string name, const string dataType){    
+  assert(validDatatype(dataType));
 
-    Table() {}
+  columns[name] = {};
+  columnNames.push_back(name);
 
-    // Add a new column to the table
-    // Ensures the datatype is supported
-    void insertColumn(const string name, const string dataType){    
-      assert(validDatatype(dataType));
+  ++numCols;
 
-      columns[name] = {};
-      columnNames.push_back(name);
+  // Populate the column with nulls for the number of rows in table
+  for (int i = 0; i < numRows; ++i){
+    columns[name].push_back(null);
+  }
+}
 
-      ++numCols;
+// Add a new row to the table
+// Skips incorrectly names/non-existent columns
+// Columns not specified get initialized as NULL
+void Table::insertRow(Row row){
+  for (string column : columnNames){
+    if (row.find(column) == row.end()) row[column] = null;;
 
-      // Populate the column with nulls for the number of rows in table
-      for (int i = 0; i < numRows; ++i){
-        columns[name].push_back(null);
+    columns[column].push_back(row[column]);
+  }
+
+  order.push_back(numRows);
+  ++numRows;
+}
+
+
+int Table::getCharTypeLength(const string name) const {
+  assert(columnNameExists(name));
+
+  return charTypeColumnLengths.at(name);
+}
+
+// Prints out the existing SQL table
+void Table::print() {
+  tableValidityCheck();
+  bool hadToCutOutValue = 0;
+  string delimiter = " | ", numHeader = "#   ";
+
+  // Header
+  cout << numHeader;
+
+  unordered_map<string, int> width;
+  int totalSize = numHeader.size();
+  for (string name : columnNames){
+    width[name] = name.size();
+    totalSize += name.size();
+    cout << delimiter << name;
+  }
+  cout << "\n";
+
+  // Separator
+  string separator(totalSize*1.5, '-'); // Why is - not monospace...
+  cout << separator << "\n";
+
+  // Rows, in order
+  for (int i : order){
+    int length = numHeader.size() - to_string(i).size();
+    string whitespace(length, ' ');
+    cout << i << whitespace;
+
+    for (string name : columnNames) {
+      Types value = columns[name][i];
+      string toPrint = "";
+
+      // Extract the value from the cell
+      if (holds_alternative<monostate>(value)) toPrint = "NULL";
+      else if (holds_alternative<string>(value)) toPrint = get<string>(value);
+      else if (holds_alternative<float>(value)){
+        ostringstream out;
+        out << fixed << setprecision(4) << get<float>(value);
+        toPrint = out.str();
       }
+      else if (holds_alternative<int>(value)) toPrint = to_string(get<int>(value));
+      else if (holds_alternative<int16_t>(value)) toPrint = to_string(get<int16_t>(value));
+      else if (holds_alternative<int64_t>(value)) toPrint = to_string(get<int64_t>(value));
+      else {
+        cerr << "Datatype of column currently unsupported by this function" << endl;
+        exit(2);
+      }
+
+      length = width[name] - toPrint.size();
+      if (length < 0) { // Edge case where value would not fit in col
+        toPrint.resize(width[name]); 
+        length = 0;
+        hadToCutOutValue = true;
+      }
+
+      string whitespace(length, ' ');
+      cout << delimiter << toPrint << whitespace;
+
     }
 
-    // Add a new row to the table
-    // Skips incorrectly names/non-existent columns
-    // Columns not specified get initialized as NULL
-    void insertRow(Row row){
-      for (string column : columnNames){
-        if (row.find(column) == row.end()) row[column] = null;;
+    cout << "\n";
 
-        columns[column].push_back(row[column]);
-      }
+  }
 
-      order.push_back(numRows);
-      ++numRows;
-    }
-
-    // Prints out the existing SQL table
-    void print() {
-      tableValidityCheck();
-      bool hadToCutOutValue = 0;
-      string delimiter = " | ", numHeader = "#   ";
-
-      // Header
-      cout << numHeader;
-
-      unordered_map<string, int> width;
-      int totalSize = numHeader.size();
-      for (string name : columnNames){
-        width[name] = name.size();
-        totalSize += name.size();
-        cout << delimiter << name;
-      }
-      cout << "\n";
-
-      // Separator
-      string separator(totalSize*1.5, '-'); // Why is - not monospace...
-      cout << separator << "\n";
-
-      // Rows, in order
-      for (int i : order){
-        int length = numHeader.size() - to_string(i).size();
-        string whitespace(length, ' ');
-        cout << i << whitespace;
-
-        for (string name : columnNames) {
-          Types value = columns[name][i];
-          string toPrint = "";
-
-          // Extract the value from the cell
-          if (holds_alternative<monostate>(value)) toPrint = "NULL";
-          else if (holds_alternative<string>(value)) toPrint = get<string>(value);
-          else if (holds_alternative<float>(value)){
-            ostringstream out;
-            out << fixed << setprecision(4) << get<float>(value);
-            toPrint = out.str();
-          }
-          else if (holds_alternative<int>(value)) toPrint = to_string(get<int>(value));
-          else if (holds_alternative<int16_t>(value)) toPrint = to_string(get<int16_t>(value));
-          else if (holds_alternative<int64_t>(value)) toPrint = to_string(get<int64_t>(value));
-          else {
-            cerr << "Datatype of column currently unsupported by this function" << endl;
-            exit(2);
-          }
-
-          length = width[name] - toPrint.size();
-          if (length < 0) { // Edge case where value would not fit in col
-            toPrint.resize(width[name]); 
-            length = 0;
-            hadToCutOutValue = true;
-          }
-
-          string whitespace(length, ' ');
-          cout << delimiter << toPrint << whitespace;
-
-        }
- 
-        cout << "\n";
-
-      }
-
-      if (hadToCutOutValue){
-        cout << "\nWARNING: Had to cut out some values since they did not fit\nConsider making your column names longer\n";
-      }
-    }
+  if (hadToCutOutValue){
+    cout << "\nWARNING: Had to cut out some values since they did not fit\nConsider making your column names longer\n";
+  }
+}
   
+// Private
 
-  private: 
-    unordered_map<string, Column> columns;
-    vector<string> columnNames; //maybe should be a set?
-    vector<int> order;
+// General validity checks
 
-    vector<string> supportedDatatypes = {
-      "INTEGER", "SMALLINT", "BIGINT", "FLOAT", "TEXT", "NULL"
-    };
+bool Table::validDatatype(const string datatype) const {
+  for (string valid : supportedDatatypes){
+    if (datatype == valid) return true;
+  }
 
-    bool validDatatype(const string datatype) const {
-      for (string valid : supportedDatatypes){
-        if (datatype == valid) return true;
-      }
+  return false;
+}
 
-      return false;
+bool Table::columnNameExists(string const name) const {
+  return find(columnNames.begin(), columnNames.end(), name) != columnNames.end();
+}
+
+// TODO: two way check: all columns in table are in columnNames,
+// all columnNames columns exist
+void Table::columnNameValidityCheck(){
+  
+}
+    
+void Table::tableValidityCheck() {
+  if (numCols != columns.size()) {
+    cerr << "numCols and actual number of columns missaligned" << endl;
+    exit(1);
+  }
+
+  for (string name : columnNames) {
+    if (numRows != columns[name].size()) {
+      cerr << "numRows and actual number of entries missaligned in " << name << endl;
+      exit(1);
     }
+  }
 
-    void tableValidityCheck() {
-      if (numCols != columns.size()) {
-        cerr << "numCols and actual number of columns missaligned" << endl;
-        exit(1);
-      }
+}
 
-      for (string name : columnNames) {
-        if (numRows != columns[name].size()) {
-          cerr << "numRows and actual number of entries missaligned in " << name << endl;
-          exit(1);
-        }
-      }
-
-    }
-
-    int numRows = 0;
-    int numCols = 0;
-};
 
 
 int main(){
---
+++ table.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <algorithm>
+
+class Table {
+public:
+    Table();
+
+    void insertColumn(const string name, const string dataType);
+    void insertRow(Row row);
+    int getCharTypeLength(const string name) const;
+    void print();
+
+private:
+    unordered_map<string, Column> columns;
+    unordered_map<string, int> charTypeColumnLengths;
+    vector<string> columnNames;
+    vector<int> order;
+
+    vector<string> supportedDatatypes = {
+        "INTEGER", "SMALLINT", "BIGINT", "FLOAT", "TEXT", "NULL"
+    };
+
+    int numRows = 0;
+    int numCols = 0;
+
+    // Validity helpers
+    bool validDatatype(const string datatype) const;
+    bool columnNameExists(const string name) const;
+    void columnNameValidityCheck();  // stub for future
+    void tableValidityCheck();
+};
