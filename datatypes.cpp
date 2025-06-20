cba9fa1b6d833d9d646630dff16daf0cae95c120
Marco Brizzio <mbrizzio@umich.edu>
2025-06-20 18:48:12 -0300

    Deep rewrite of comparators, implement custom Types vs. template T (and viceversa) comparators, as well as Types vs. Types comparators

--- datatypes.cpp
+++ datatypes.cpp
@@ -133,405 +133,210 @@ ostream& operator<<(ostream& os, const Types& self){
 
 /////////////////////// Comparator hell (all classes) ///////////////////
 
-////// Types vs. Anything
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator==(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value == rhs;
-  }, lhs);
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator!=(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value != rhs;
-  }, lhs);
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value < rhs;
-  }, lhs);
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<=(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value <= rhs;
-  }, lhs);
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>=(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value >= rhs;
-  }, lhs);
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>(const StrictTypes& lhs, const T& rhs) {
-  return std::visit([&](const auto& value) -> bool {
-    return value > rhs;
-  }, lhs);
-}
-
-
-////// Anything vs. Types
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator==(const T& lhs, const StrictTypes& rhs) {
-  return rhs == lhs;
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator!=(const T& lhs, const StrictTypes& rhs) {
-  return rhs != lhs;
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>(const T& lhs, const StrictTypes& rhs) {
-  return rhs > lhs;
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>=(const T& lhs, const StrictTypes& rhs) {
-  return rhs >= lhs;
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<=(const T& lhs, const StrictTypes& rhs) {
-  return rhs <= lhs;
-}
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<(const T& lhs, const StrictTypes& rhs) {
-  return rhs < lhs;
-}
-
-
-////// Types vs. Types
-template<typename T>
-STRICTLY_TYPES(T)
-operator==(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val == rhs_val; 
-  }, lhs, rhs);
-}
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator!=(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val != rhs_val; 
-  }, lhs, rhs);
-}
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator<(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val < rhs_val; 
-  }, lhs, rhs);
-}
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator<=(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val <= rhs_val; 
-  }, lhs, rhs);
-}
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator>(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val > rhs_val; 
-  }, lhs, rhs);
-}
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator>=(const T& lhs, const T& rhs){
-  return std::visit([&](const auto& lhs_val, const auto& rhs_val) -> bool {
-    return lhs_val >= rhs_val; 
-  }, lhs, rhs);
-}
-
-
-////// Anything vs. Monostate
-template<typename T>
-bool operator==(const monostate&, const T &_){
-  return false;
-}
-template<typename T>
-bool operator!=(const monostate&, const T &_){
-  return true;
-}
-template<typename T>
-bool operator<(const monostate&, const T &_){
-  return false;
-}
-template<typename T>
-bool operator<=(const monostate&, const T &_){
-  return false;
-}
-template<typename T>
-bool operator>(const monostate&, const T &_){
-  return false;
-}
-template<typename T>
-bool operator>=(const monostate&, const T &_){
-  return false;
-}
-
-
-////// Monostate vs. Anything
-template<typename T>
-bool operator==(const T &_, const monostate&){
-  return false;
-}
-template<typename T>
-bool operator!=(const T &_, const monostate&){
-  return true;
-}
-template<typename T>
-bool operator<(const T &_, const monostate&){
-  return false;
-}
-template<typename T>
-bool operator<=(const T &_, const monostate&){
-  return false;
-}
-template<typename T>
-bool operator>(const T &_, const monostate&){
-  return false;
-}
-template<typename T>
-bool operator>=(const T &_, const monostate&){
-  return false;
-}
-
 ////// Varchar vs. Varchar
-bool Varchar::operator==(const Varchar &rhs){
+bool Varchar::operator==(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::equal_to<>{});
 }
 
-bool Varchar::operator!=(const Varchar &rhs){
+bool Varchar::operator!=(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::not_equal_to<>{});
 }
 
-bool Varchar::operator<(const Varchar &rhs){
+bool Varchar::operator<(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::less<>{});
 }
 
-bool Varchar::operator>(const Varchar &rhs){
+bool Varchar::operator>(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::greater<>{});
 }
 
-bool Varchar::operator<=(const Varchar &rhs){
+bool Varchar::operator<=(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::less_equal<>{});
 }
 
-bool Varchar::operator>=(const Varchar &rhs){
+bool Varchar::operator>=(const Varchar &rhs) const {
   return comparatorHelper(value, rhs.value, std::greater_equal<>{});
 }
 
 
 ////// Varchar vs. String
-bool Varchar::operator==(const string &rhs){
+bool Varchar::operator==(const string &rhs) const {
   return comparatorHelper(value, rhs, std::equal_to<>{});
 } 
 
-bool Varchar::operator!=(const string &rhs){
+bool Varchar::operator!=(const string &rhs) const {
   return comparatorHelper(value, rhs, std::not_equal_to<>{});
 }
 
-bool Varchar::operator<(const string &rhs){
+bool Varchar::operator<(const string &rhs) const {
   return comparatorHelper(value, rhs, std::less<>{});
 }
 
-bool Varchar::operator>(const string &rhs){
+bool Varchar::operator>(const string &rhs) const {
   return comparatorHelper(value, rhs, std::greater<>{});
 }
 
-bool Varchar::operator<=(const string &rhs){
+bool Varchar::operator<=(const string &rhs) const {
   return comparatorHelper(value, rhs, std::less_equal<>{});
 }
 
-bool Varchar::operator>=(const string &rhs){
+bool Varchar::operator>=(const string &rhs) const {
   return comparatorHelper(value, rhs, std::greater_equal<>{});
 } 
 
 
 ///// String vs. Varchar
-bool operator==(const string &lhs, const Varchar &rhs){
+bool operator==(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::equal_to<>{});
 }
 
-bool operator!=(const string &lhs, const Varchar &rhs){
+bool operator!=(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::not_equal_to<>{});
 }
 
-bool operator<(const string &lhs, const Varchar &rhs){
+bool operator<(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::less<>{});
 }
 
-bool operator>(const string &lhs, const Varchar &rhs){
+bool operator>(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::greater<>{});
 }
 
-bool operator<=(const string &lhs, const Varchar &rhs){
+bool operator<=(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::less_equal<>{});
 }
 
-bool operator>=(const string &lhs, const Varchar &rhs){
+bool operator>=(const string &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.value, std::greater_equal<>{});
 }
 
 
 ///// SQLChar vs. SQLChar
-bool SQLChar::operator==(const SQLChar &rhs){
+bool SQLChar::operator==(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::equal_to<>{});
 }
 
-bool SQLChar::operator!=(const SQLChar &rhs){
+bool SQLChar::operator!=(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::not_equal_to<>{});
 }
 
-bool SQLChar::operator<(const SQLChar &rhs){
+bool SQLChar::operator<(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::less<>{});
 }
 
-bool SQLChar::operator>(const SQLChar &rhs){
+bool SQLChar::operator>(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::greater<>{});
 }
 
-bool SQLChar::operator<=(const SQLChar &rhs){
+bool SQLChar::operator<=(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::less_equal<>{});
 }
 
-bool SQLChar::operator>=(const SQLChar &rhs){
+bool SQLChar::operator>=(const SQLChar &rhs) const {
   return comparatorHelper(value, rhs.value, std::greater_equal<>{});
 }
 
 
 ///// String vs. SQLChar
-bool operator==(const string &lhs, const SQLChar &rhs){
+bool operator==(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::equal_to<>{});
 }
 
-bool operator!=(const string &lhs, const SQLChar &rhs){
+bool operator!=(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::not_equal_to<>{});
 }
 
-bool operator<(const string &lhs, const SQLChar &rhs){
+bool operator<(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::less<>{});
 }
 
-bool operator>(const string &lhs, const SQLChar &rhs){
+bool operator>(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::greater<>{});
 }
 
-bool operator<=(const string &lhs, const SQLChar &rhs){
+bool operator<=(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::less_equal<>{});
 }
 
-bool operator>=(const string &lhs, const SQLChar &rhs){
+bool operator>=(const string &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs, rhs.getUnpaddedValue(), std::greater_equal<>{});
 }
 
 
 ////// SQLChar vs. String
-bool SQLChar::operator==(const string &rhs){
+bool SQLChar::operator==(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::equal_to<>{});
 } 
 
-bool SQLChar::operator!=(const string &rhs){
+bool SQLChar::operator!=(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::not_equal_to<>{});
 }
 
-bool SQLChar::operator<(const string &rhs){
+bool SQLChar::operator<(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::less<>{});
 }
 
-bool SQLChar::operator>(const string &rhs){
+bool SQLChar::operator>(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::greater<>{});
 }
 
-bool SQLChar::operator<=(const string &rhs){
+bool SQLChar::operator<=(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::less_equal<>{});
 }
 
-bool SQLChar::operator>=(const string &rhs){
+bool SQLChar::operator>=(const string &rhs) const {
   return comparatorHelper(getUnpaddedValue(), rhs, std::greater_equal<>{});
 } 
 
 
 
 ////// Varchar vs. SQLChar
-bool operator==(const Varchar &lhs, const SQLChar &rhs){
+bool operator==(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::equal_to<>{});
 }
 
-bool operator!=(const Varchar &lhs, const SQLChar &rhs){
+bool operator!=(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::not_equal_to<>{});
 }
 
-bool operator<(const Varchar &lhs, const SQLChar &rhs){
+bool operator<(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::less<>{});
 }
 
-bool operator>(const Varchar &lhs, const SQLChar &rhs){
+bool operator>(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::greater<>{});
 }
 
-bool operator<=(const Varchar &lhs, const SQLChar &rhs){
+bool operator<=(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::less_equal<>{});
 }
 
-template<typename StrictVarchar, typename StrictSQLChar>
-STRICT_SQLCHAR_VARCHAR(StrictVarchar, StrictSQLChar)
-operator>=(const StrictVarchar &lhs, const StrictSQLChar &rhs){
+bool operator>=(const Varchar &lhs, const SQLChar &rhs) {
   return rhs.comparatorHelper(lhs.value, rhs.getUnpaddedValue(), std::greater_equal<>{});
 }
 
 ////// SQLChar vs. Varchar
-bool operator==(const SQLChar &lhs, const Varchar &rhs){
+bool operator==(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::equal_to<>{});
 }
 
-bool operator!=(const SQLChar &lhs, const Varchar &rhs){
+bool operator!=(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::not_equal_to<>{});
 }
 
-bool operator<(const SQLChar &lhs, const Varchar &rhs){
+bool operator<(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::less<>{});
 }
 
-bool operator>(const SQLChar &lhs, const Varchar &rhs){
+bool operator>(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::greater<>{});
 }
 
-bool operator<=(const SQLChar &lhs, const Varchar &rhs){
+bool operator<=(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::less_equal<>{});
 }
 
-template<typename StrictVarchar, typename StrictSQLChar>
-STRICT_SQLCHAR_VARCHAR(StrictVarchar, StrictSQLChar)
-operator>=(const StrictSQLChar &lhs, const StrictVarchar &rhs){
+bool operator>=(const SQLChar &lhs, const Varchar &rhs) {
   return rhs.comparatorHelper(lhs.getUnpaddedValue(), rhs.value, std::greater_equal<>{});
 }
 
--- datatypes.h
+++ datatypes.h
@@ -56,19 +56,19 @@ class Varchar{
       return comp(lhs, rhs);
     }
 
-    bool operator==(const Varchar &rhs);
-    bool operator!=(const Varchar &rhs);
-    bool operator<(const Varchar &rhs);
-    bool operator>(const Varchar &rhs);
-    bool operator<=(const Varchar &rhs);
-    bool operator>=(const Varchar &rhs); 
+    bool operator==(const Varchar &rhs) const;
+    bool operator!=(const Varchar &rhs) const;
+    bool operator<(const Varchar &rhs) const;
+    bool operator>(const Varchar &rhs) const;
+    bool operator<=(const Varchar &rhs) const;
+    bool operator>=(const Varchar &rhs) const; 
 
-    bool operator==(const string &rhs);
-    bool operator!=(const string &rhs);
-    bool operator<(const string &rhs);
-    bool operator>(const string &rhs);
-    bool operator<=(const string &rhs);
-    bool operator>=(const string &rhs); 
+    bool operator==(const string &rhs) const;
+    bool operator!=(const string &rhs) const;
+    bool operator<(const string &rhs) const;
+    bool operator>(const string &rhs) const;
+    bool operator<=(const string &rhs) const;
+    bool operator>=(const string &rhs) const; 
 
     friend bool operator==(const string &lhs, const Varchar &rhs);
     friend bool operator!=(const string &lhs, const Varchar &rhs);
@@ -97,19 +97,19 @@ class SQLChar : public Varchar {
     SQLChar(Table* table, string name, string Value);
 
     // resolve ambiguity by  overloading the parent comparators
-    bool operator==(const SQLChar &rhs);
-    bool operator!=(const SQLChar &rhs);
-    bool operator<(const SQLChar &rhs);
-    bool operator>(const SQLChar &rhs);
-    bool operator<=(const SQLChar &rhs);
-    bool operator>=(const SQLChar &rhs);
+    bool operator==(const SQLChar &rhs) const;
+    bool operator!=(const SQLChar &rhs) const;
+    bool operator<(const SQLChar &rhs) const;
+    bool operator>(const SQLChar &rhs) const;
+    bool operator<=(const SQLChar &rhs) const;
+    bool operator>=(const SQLChar &rhs) const;
     
-    bool operator==(const string &rhs);
-    bool operator!=(const string &rhs);
-    bool operator<(const string &rhs);
-    bool operator>(const string &rhs);
-    bool operator<=(const string &rhs);
-    bool operator>=(const string &rhs); 
+    bool operator==(const string &rhs) const;
+    bool operator!=(const string &rhs) const;
+    bool operator<(const string &rhs) const;
+    bool operator>(const string &rhs) const;
+    bool operator<=(const string &rhs) const;
+    bool operator>=(const string &rhs) const; 
     
     friend bool operator==(const string &lhs, const SQLChar &rhs);
     friend bool operator!=(const string &lhs, const SQLChar &rhs);
@@ -123,12 +123,7 @@ class SQLChar : public Varchar {
     friend bool operator<(const Varchar &lhs, const SQLChar &rhs);
     friend bool operator>(const Varchar &lhs, const SQLChar &rhs);
     friend bool operator<=(const Varchar &lhs, const SQLChar &rhs);
-    template<typename StrictVarchar, typename StrictSQLChar>
-    std::enable_if_t< 
-      std::is_same_v<std::decay_t<StrictVarchar>, Varchar> && 
-      std::is_same_v<std::decay_t<StrictSQLChar>, SQLChar>, 
-    bool> 
-    friend operator>=(const StrictVarchar &lhs, const StrictSQLChar &rhs);
+    friend bool operator>=(const Varchar &lhs, const SQLChar &rhs);
 
     friend bool operator==(const SQLChar &lhs, const Varchar &rhs);
     friend bool operator!=(const SQLChar &lhs, const Varchar &rhs);
@@ -156,84 +151,238 @@ using Types = variant<
   monostate   //NULL
 >;
 
-////// Types vs. Anything
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator==(const StrictTypes& lhs, const T& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator!=(const StrictTypes& lhs, const T& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<(const StrictTypes& lhs, const T& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<=(const StrictTypes& lhs, const T& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>=(const StrictTypes& lhs, const T& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>(const StrictTypes& lhs, const T& rhs);
-
-////// Anything vs. Types
-template<typename T, typename StrictTypes> 
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes) 
-operator==(const T& lhs, const StrictTypes& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator!=(const T& lhs, const StrictTypes& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>(const T& lhs, const StrictTypes& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator>=(const T& lhs, const StrictTypes& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<=(const T& lhs, const StrictTypes& rhs);
-
-template<typename T, typename StrictTypes>
-STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
-operator<(const T& lhs, const StrictTypes& rhs);
-
-
-////// Types vs. Types
-template<typename T> 
-STRICTLY_TYPES(T) 
-operator==(const T& lhs, const T& rhs);
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator!=(const T& lhs, const T& rhs);
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator<(const T& lhs, const T& rhs);
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator<=(const T& lhs, const T& rhs);
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator>(const T& lhs, const T& rhs);
-
-template<typename T>
-STRICTLY_TYPES(T)
-operator>=(const T& lhs, const T& rhs);
-
-
 ostream& operator<<(ostream& os, const Types& self);
 
 using Row = unordered_map<string, Types>;
 using Column = vector<Types>;
+
+
+/////////////// Comparator hell part 2 ////////////////////////////
+////// Types vs. Anything
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator==(const StrictTypes& lhs, const T& rhs) {
+    if (const T* pval = std::get_if<T>(&lhs)) {
+        return *pval == rhs;
+    }
+    return false;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator!=(const StrictTypes& lhs, const T& rhs) {
+    if (const T* pval = std::get_if<T>(&lhs)) {
+        return *pval != rhs;
+    }
+    return true;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator<(const StrictTypes& lhs, const T& rhs) {
+    if (const T* pval = std::get_if<T>(&lhs)) {
+        return *pval < rhs;
+    }
+    return false;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator>(const StrictTypes& lhs, const T& rhs) {
+    if (const T* pval = std::get_if<T>(&lhs)) {
+        return *pval > rhs;
+    }
+    return false;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator<=(const StrictTypes& lhs, const T& rhs) {
+    if (const T* pval = std::get_if<T>(&lhs)) {
+        return *pval <= rhs;
+    }
+    return false;
+}
+
+////// Anything vs. Types
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator==(const T& lhs, const StrictTypes& rhs) {
+  return rhs == lhs;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator!=(const T& lhs, const StrictTypes& rhs) {
+  return rhs != lhs;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator>(const T& lhs, const StrictTypes& rhs) {
+  return rhs > lhs;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator>=(const T& lhs, const StrictTypes& rhs) {
+  return rhs >= lhs;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator<=(const T& lhs, const StrictTypes& rhs) {
+  return rhs <= lhs;
+}
+
+template<typename T, typename StrictTypes>
+STRICTLY_NOT_TYPES_AND_STRICTLY_TYPES(T, StrictTypes)
+operator<(const T& lhs, const StrictTypes& rhs) {
+  return rhs < lhs;
+}
+
+////// Anything vs. Monostate
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool> 
+operator==(const monostate&, const T &_) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool> 
+operator!=(const monostate&, const T &_) {
+  return true;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool> 
+operator<(const monostate&, const T &_) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool> 
+operator<=(const monostate&, const T &_) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>
+operator>(const monostate&, const T &_) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool> 
+operator>=(const monostate&, const T &_) {
+  return false;
+}
+
+
+////// Monostate vs. Anything
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator==(const T &_, const monostate&) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator!=(const T &_, const monostate&) {
+  return true;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator<(const T &_, const monostate&) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator<=(const T &_, const monostate&) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator>(const T &_, const monostate&) {
+  return false;
+}
+template<typename T>
+enable_if_t<!is_same_v<decay_t<T>, monostate>, bool>  
+operator>=(const T &_, const monostate&) {
+  return false;
+}
+
+
+
+// Monostate vs. Monostate
+// bool operator==(const monostate &a, const monostate &b){
+//   return true;
+// }
+
+// bool operator!=(const monostate &a, const monostate &b){
+//   return false;
+// }
+
+// bool operator<(const monostate &a, const monostate &b){
+//   return false;
+// }
+
+// bool operator>(const monostate &a, const monostate &b){
+//   return false;
+// }
+
+// bool operator<=(const monostate &a, const monostate &b){
+//   return false;
+// }
+
+// bool operator>=(const monostate &a, const monostate &b){
+//   return false;
+// }
+
+// Copy-pasting code like this feels AWFUL; think of an alternativ way of doinh this
+template <typename Comparator>
+struct GenericTypesVisitor {
+    Comparator comp;
+    bool mismatchValue;
+
+    GenericTypesVisitor(Comparator Comp, bool Value) 
+        : comp(Comp), mismatchValue(Value) {}
+
+    template <typename T1, typename T2>
+    bool operator()(const T1& lhs, const T2& rhs) const {
+        // USE THIS CHAINED STRUCTURE
+        if constexpr (std::is_same_v<T1, T2>) {
+            return comp(lhs, rhs);
+        }
+        else if constexpr (
+            (std::is_same_v<T1, Varchar> && std::is_same_v<T2, SQLChar>) ||
+            (std::is_same_v<T1, SQLChar> && std::is_same_v<T2, Varchar>)
+        ) {
+            return comp(lhs, rhs);
+        }
+        else if constexpr (std::is_arithmetic_v<T1> && std::is_arithmetic_v<T2>) {
+            return comp(static_cast<double>(lhs), static_cast<double>(rhs));
+        }
+        else {
+            return mismatchValue;
+        }
+    }
+};
+
+inline bool operator==(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::equal_to<>{}, false}, lhs, rhs);
+}
+
+inline bool operator!=(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::not_equal_to<>{}, true}, lhs, rhs);
+}
+
+inline bool operator<(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::less<>{}, false}, lhs, rhs);
+}
+
+inline bool operator>(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::greater<>{}, false}, lhs, rhs);
+}
+
+inline bool operator<=(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::less_equal<>{}, false}, lhs, rhs);
+}
+
+inline bool operator>=(const Types& lhs, const Types& rhs) {
+    return std::visit(GenericTypesVisitor{std::greater_equal<>{}, false}, lhs, rhs);
+}
